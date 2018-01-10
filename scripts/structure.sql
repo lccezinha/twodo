bdfec876f80cd73e8a2531afc18271a6fd58192e
Luiz Cezer <lccezinha@gmail.com>
2018-01-10 21:56:45 -0200

    Change database structure

--- scripts/structure.sql
+++ scripts/structure.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS todos (
   id SERIAL PRIMARY KEY,
-  title VARCHAR,
-  description VARCHAR,
+  title VARCHAR(100),
+  description VARCHAR(500),
   created_at TIMESTAMP
 );
 
