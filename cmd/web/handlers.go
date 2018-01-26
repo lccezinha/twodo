fe766cc06400ba7fd98b41bcad800af22c4e41f8
Luiz Cezer <lccezinha@gmail.com>
2018-01-26 20:44:52 -0200

    Add basic index request

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,12 +10,22 @@ var errTemplateNotFound = errors.New("Template not found")
 
 // IndexHandler will handle request to "/" path
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	template, err := template.ParseFiles("../../views/index.html")
 
 	if err != nil {
 		panic(errTemplateNotFound)
 	}
 
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	template.Execute(w, nil)
+}
+
+// CreateHandler will handle request to "/" path with post request
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.Header().Set("Error Message", "Use wrong http method")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,8 @@ package web
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -15,4 +17,34 @@ func TestIndexHandler(t *testing.T) {
 	if status := w.Code; status != http.StatusOK {
 		t.Errorf("Handler returning wrong http status code, expected %v, received %v", http.StatusOK, w.Code)
 	}
+}
+
+func TestCreateHandler(t *testing.T) {
+	t.Run("Fail - use wrong http method to send request", func(t *testing.T) {
+		data := url.Values{}
+		data.Add("title", "")
+		data.Add("description", "Some Description")
+		req := httptest.NewRequest("GET", "/", strings.NewReader(data.Encode()))
+		w := httptest.NewRecorder()
+		handler := http.HandlerFunc(CreateHandler)
+		handler.ServeHTTP(w, req)
+
+		if status := w.Code; status != http.StatusMethodNotAllowed {
+			t.Errorf("Handler returning wrong http status code, expected %v, received %v", http.StatusBadRequest, w.Code)
+		}
+	})
+
+	// t.Run("Fail because todo title is not set", func(t *testing.T) {
+	// 	data := url.Values{}
+	// 	data.Add("title", "")
+	// 	data.Add("description", "Some Description")
+	// 	req := httptest.NewRequest("POST", "/", strings.NewReader(data.Encode()))
+	// 	w := httptest.NewRecorder()
+	// 	handler := http.HandlerFunc(CreateHandler)
+	// 	handler.ServeHTTP(w, req)
+	//
+	// 	if status := w.Code; status != http.StatusBadRequest {
+	// 		t.Errorf("Handler returning wrong http status code, expected %v, received %v", http.StatusBadRequest, w.Code)
+	// 	}
+	// })
 }
