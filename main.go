c5dfba5ff17e40f9510cca78e07091ffa9f93276
Luiz Cezer <lccezinha@gmail.com>
2018-03-29 21:52:43 -0300

    Clean up main.go

--- main.go
+++ main.go
@@ -3,21 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/lccezinha/twodo/cmd/web"
 )
 
 func main() {
-	http.HandleFunc("/", web.IndexHandler)
-	http.HandleFunc("/todos", web.CreateHandler)
-	http.HandleFunc("/destroy", web.DestroyHandler)
-	http.HandleFunc("/done", web.DoneHandler)
-	http.HandleFunc("/undone", web.UndoneHandler)
-
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	log.Print("Running server... http://localhost:8080")
-
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
